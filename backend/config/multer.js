67684cac36a380a7fcbe900b5de5de4e54e03526
KEERTHIGA-TS <23cseb16keerthigats@gmail.com>
2025-12-20 22:57:28 +0530

    Update multer.js

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -1,13 +1,15 @@
-const multer = require('multer');
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('./cloudinary');
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("./cloudinary");
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: {
-    folder: "megamart",
-    resource_type: "image",
-    allowed_formats: ['jpg', 'jpeg', 'png'],
+  params: async (req, file) => {
+    return {
+      folder: "megamart",
+      resource_type: "image",
+      format: file.mimetype.split("/")[1], // png / jpg
+    };
   },
 });
 
